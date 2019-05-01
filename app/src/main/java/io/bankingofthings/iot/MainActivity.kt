37096240d5dfb3bf34621057efa15caf35f1dc38
Ercan Bozoglu <ercan@bankingofthings.io>
2019-05-01 11:32:12 +0200

    Removed example code

--- app/src/main/java/io/bankingofthings/iot/MainActivity.kt
+++ app/src/main/java/io/bankingofthings/iot/MainActivity.kt
@@ -45,9 +45,6 @@ class MainActivity : Activity() {
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var finn: Finn
-    private var servo: Servo? = null
-    private var startEngineDisposable: Disposable? = null
-    var goingUp = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,21 +52,6 @@ class MainActivity : Activity() {
 
 
         startFinn()
-    }
-
-    private fun setupEngine() {
-        try {
-            servo = Servo("PWM0", 50.0)
-                .apply {
-                    setEnabled(true)
-                    angle = 90.0
-                }
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-
-        ButtonInputDriver("BCM21", Button.LogicState.PRESSED_WHEN_LOW, KeyEvent.KEYCODE_SPACE)
-            .apply { register() }
     }
 
     /**
@@ -84,70 +66,11 @@ class MainActivity : Activity() {
              * When user has paired the device with QR, Bluetooth or NFC
              */
             override fun onDevicePaired() {
-                setupEngine()
-                startEngine()
+                System.out.println("MainActivity:onDevicePaired device ready to trigger actions")
             }
         })
 
         binding.qrHolder.setImageBitmap(finn.qrBitmap)
-    }
-
-    override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
-        startEngine()
-        return super.onKeyUp(keyCode, event)
-    }
-
-
-    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        System.out.println("MainActivity:onKeyDown")
-
-        if (startEngineDisposable?.isDisposed == false) {
-            startEngineDisposable?.dispose()
-            System.out.println("MainActivity:onKeyDown ${servo?.angle}")
-
-            finn.triggerAction("248DF988-B811-418B-83BF-F55F5B46EEAB", "my license plate")
-                .subscribe(
-                    {
-                        System.out.println("MainActivity:onKeyDown triggered")
-                    }
-                    , {
-                        it.printStackTrace()
-
-                        when (it::class) {
-                            HttpException::class -> {
-                                if ((it as HttpException).code() == 400) {
-                                    System.out.println("MainActivity:onKeyDown trigger not active")
-                                }
-                            }
-                        }
-
-                    }
-                )
-        }
-
-        return super.onKeyDown(keyCode, event)
-    }
-
-    private fun startEngine() {
-        startEngineDisposable?.dispose()
-
-        startEngineDisposable = Observable.interval(10, TimeUnit.MILLISECONDS)
-            .map {
-                if (goingUp) {
-                    servo?.angle = servo?.angle!! + 1
-
-                    if (servo?.angle!! >= 180.0) {
-                        goingUp = false
-                    }
-                } else {
-                    servo?.angle = servo?.angle!! - 1
-
-                    if (servo?.angle!! <= 0.0) {
-                        goingUp = true
-                    }
-                }
-            }
-            .subscribe({}, { it.printStackTrace() })
     }
 
     /**
